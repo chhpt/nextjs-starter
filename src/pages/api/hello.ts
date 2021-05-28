cc3df167f803f0891c2253270fab6ba2b60e62f2
chhpt <cwuyiqing@gmail.com>
2021-05-28 15:58:11 +0800

    chore: code format

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200;
-  res.json({ name: `John Doe` });
-};
+  res.statusCode = 200
+  res.json({ name: `John Doe` })
+}
