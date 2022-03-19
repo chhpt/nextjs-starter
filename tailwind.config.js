6e510e4727cb1c83dc5028068a155d98d4113106
chhpt <cwuyiqing@gmail.com>
2022-03-19 16:13:18 +0800

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  mode: 'jit',
   darkMode: false,
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
