de869a620d25762f9ca900ff0104474cbfb0232f
easyvipin <vipinchandra555@gmail.com>
2023-03-10 08:53:20 +0530

    feature to add custom layout per page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { Layout } from '@/components'
 import '@/styles/global.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
+
+  return getLayout(<Component {...pageProps} />)
 }
