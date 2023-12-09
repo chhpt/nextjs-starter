eea9a717271db1c55a0021b8d1c5351638dff9d7
chhpt <cwuyiqing@gmail.com>
2023-12-09 14:13:02 +0000

    feat: update eslint and fix lint issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
-import { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
 import { Layout } from '@/components'
 import '@/styles/global.css'
 
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req: NextApiRequest, res: NextApiResponse) => {
